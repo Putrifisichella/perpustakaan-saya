7694d466b3b9fd7fb57303fa6f359c63b541f2b9
Putrifisichella <2210631170096@student.ac.id>
2024-06-19 22:04:09 +0700

    Progres pertama CRUD + autentikasi

---
+++ tambah-staff.php
@@ -0,0 +1,48 @@
+<?php 
+    include_once("./connect.php");
+
+    if(isset($_POST ["submit"])) {
+        $nama =  $_POST["nama"];
+        $telp =  $_POST["telp"];
+        $email =  $_POST["email"];
+
+        $query = mysqli_query($db, "INSERT INTO staff VALUES (
+            NULL, '$nama', '$telp', '$email'
+        )");
+
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FORM TAMBAH Staff</title>
+</head>
+<body>
+    <h1>Form Tambah Data Staff</h1>
+
+    <form action = "" method="POST">
+        <label for="nama">Nama</label>
+        <input type="text" id="nama" name="nama">
+
+        <br>
+        <br>
+
+        <label for="telp">Telp</label>
+        <input type="text" id="telp" name="telp">
+
+        <br>
+        <br>
+
+        <label for="email">Email</label>
+        <input type="email" id="email" name="email">
+
+        <br>
+        <br>
+        <button type="submit" name="submit">SUBMIT</button>
+    </form>
+        <br>
+        <a href="./staff.php">Kembali ke halaman staff</a>
+</body>
+</html>
